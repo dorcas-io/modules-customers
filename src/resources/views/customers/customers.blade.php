4bb47559220716213e24efd33e557a3601bf0940
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-06-24 00:25:29 +0100

    fixes

--- src/resources/views/customers/customers.blade.php
+++ src/resources/views/customers/customers.blade.php
@@ -168,65 +168,7 @@
 
         },
 
-        /*clickAction: function (event) {
-            console.log(event.target);
-            var target = event.target.tagName.toLowerCase() === 'i' ? event.target.parentNode : event.target;
-            var attrs = Hub.utilities.getElementAttributes(target);
-                // get the attributes
-                var classList = target.classList;
-                if (classList.contains('view')) {
-                    return true;
-                } else if (classList.contains('remove')) {
-                    this.delete(attrs);
-                }
-            },
-            delete: function (attributes) {
-                console.log(attributes);
-                var name = attributes['data-name'] || '';
-                var id = attributes['data-id'] || null;
-                if (id === null) {
-                    return false;
-                }
-                context = this;
-                swal({
-                    title: "Are you sure?",
-                    text: "You are about to delete " + name + " from your contacts.",
-                    type: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "Yes, delete it!",
-                    closeOnConfirm: false,
-                    showLoaderOnConfirm: true
-                }, function() {
-                    axios.delete("/xhr/crm/customers/" + id)
-                    .then(function (response) {
-                        console.log(response);
-                        context.visible = false;
-                        context.contactsCount -= 1;
-                        $('#contacts-table').bootstrapTable('removeByUniqueId', response.data.data.id);
-                        return swal("Deleted!", "The customer was successfully deleted.", "success");
-                    })
-                    .catch(function (error) {
-                        var message = '';
-                        console.log(error);
-                        if (error.response) {
-                                // The request was made and the server responded with a status code
-                                // that falls out of the range of 2xx
-                                var e = error.response.data.errors[0];
-                                message = e.title;
-                            } else if (error.request) {
-                                // The request was made but no response was received
-                                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                                // http.ClientRequest in node.js
-                                message = 'The request was made but no response was received';
-                            } else {
-                                // Something happened in setting up the request that triggered an Error
-                                message = error.message;
-                            }
-                            return swal("Delete Failed", message, "warning");
-                        });
-                });
-            },*/
+        
             triggerEdit: function () {
                 if (this.request_id.length === 0 || this.caccounts.length === 0) {
                     return '';
